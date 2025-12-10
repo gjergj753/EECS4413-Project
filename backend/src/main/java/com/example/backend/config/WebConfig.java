9e7bccfb2ec8eaa96001004d164e8b37c19bcc1f
Domenico Calautti <domeniccalautti@gmail.com>
2025-12-09 19:43:35 -0500

    deployment configs added

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.*;
 public class WebConfig implements WebMvcConfigurer {
     @Override public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+                .allowedOrigins("http://localhost", "http://localhost:5173", "http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
