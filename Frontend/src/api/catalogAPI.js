555204f9f0f28a2dbdad6cc49d8e29f4284ace32
Domenico Calautti <domeniccalautti@gmail.com>
2025-12-10 21:48:27 -0500

    Remove basic auth from public catalog endpoints

--- Frontend/src/api/catalogAPI.js
+++ Frontend/src/api/catalogAPI.js
@@ -1,36 +1,34 @@
 import API_BASE_URL from "./api";
 import axios from "axios";
 
+
 export async function getGenres() {
-    return axios.get(`${API_BASE_URL}/catalog/genres`, {
-        auth: {
-            username: "admin@test.com",
-            password: "Pass123!"
-        }
-    })
+    return axios
+        .get(`${API_BASE_URL}/catalog/genres`)
         .then(res => res.data)
         .catch(err => { throw err; });
 }
 
 
-export async function listBooks({ page = 0, size = 20, sort = "title", search = "", genre = "" }) {
-  return axios.get(`${API_BASE_URL}/catalog/books`, {
-    params: { page, size, sort, search, genre },
-    auth: {
-      username: "admin@test.com",
-      password: "Pass123!"
-    }
-  })
-  .then(res => res.data)
-  .catch(err => { throw err; });
+export async function listBooks({
+                                    page = 0,
+                                    size = 20,
+                                    sort = "title",
+                                    search = "",
+                                    genre = ""
+                                }) {
+    return axios
+        .get(`${API_BASE_URL}/catalog/books`, {
+            params: { page, size, sort, search, genre }
+        })
+        .then(res => res.data)
+        .catch(err => { throw err; });
 }
 
 
 export async function fetchBookById(id) {
-  return axios.get(`${API_BASE_URL}/catalog/books/${id}`, {
-    auth: {
-      username: "admin@test.com",
-      password: "Pass123!"
-    }
-  }).then(res => res.data);
+    return axios
+        .get(`${API_BASE_URL}/catalog/books/${id}`)
+        .then(res => res.data)
+        .catch(err => { throw err; });
 }
